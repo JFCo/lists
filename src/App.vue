9f57b0bb96959959cbc1c0767050c332b51f666e
Jason Funderburk <jfco.us@gmail.com>
2018-07-06 17:13:38 -0400

    listmaker trial

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Listmaker/>
+  </div>
+</template>
+
+<script>
+import Listmaker from './components/Listmaker'
+import Vue from 'vue'
+import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+
+Vue.use(Buefy)
+
+export default {
+  name: 'App',
+  components: {
+    Listmaker
+  }
+}
+</script>
+
+<style>
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
